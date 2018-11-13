e722cfc362a74605e02f0a55e163436c6e4f0e6f
Kamil Ganski <kamilganski2@gmail.com>
2018-11-13 10:45:28 +0100

    After first class and methods

---
+++ src/tutorial/PersonTest.java
@@ -0,0 +1,30 @@
+package tutorial;
+
+import java.util.logging.Logger;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class PersonTest {
+
+    @Test
+    public void testPerson() {
+
+        Person p = new Person("Mike", 31, 178, 77, "Brown", "MALE");
+        Logger l = Logger.getLogger(Person.class.getName());
+
+        l.info("Name: " + p.getName());
+        l.info("Age: " + p.getAge());
+        l.info("Height (cm): "+ p.getHeight());
+        l.info("Weight (kg): " + p.getWeight());
+        l.info("Eye Color: " + p.getEyeColor());
+        l.info("Gender: " + p.getGender());
+
+        assertEquals("Mike", p.getName());
+        assertEquals(31, p.getAge());
+        assertEquals(178, p.getHeight());
+        assertEquals(77, p.getWeight());
+        assertEquals("Brown", p.getEyeColor());
+        assertEquals("MALE", p.getGender());
+    }
+}
